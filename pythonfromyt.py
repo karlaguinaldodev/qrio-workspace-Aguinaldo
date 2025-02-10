081bf5fe3334d3c30d1d5ccd108c1d6af29eb76c
karlaguinaldodev <aguinaldokarl.al@gmail.com>
2025-02-10 17:14:55 +0800

    Practice from youtube (Programming with mosh (6 hours Vid))

--- pythonfromyt.py
+++ pythonfromyt.py
@@ -141,5 +141,11 @@
 #     uniques.append(number)
 # print(uniques)
 
-
+#i understand it now
+numbers = [5, 5, 5, 5, 5, 2, 1, 7, 4, 4, 4, 4]
+uniques = []
+for number in numbers:
+  if number not in uniques:
+    uniques.append(number)
+print(uniques)
 
